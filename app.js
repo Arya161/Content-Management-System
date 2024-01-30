50a2534bf302b0a839adfbc017ab6a78511be0b8
Arya Parolia <99472244+Arya161@users.noreply.github.com>
2024-01-30 10:40:16 +0530

    Added more modules and views

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const exphbs = require('express-handlebars');
 
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('handlebars', exphbs.engine({defaultLayout: 'home'}));//newer version of Js expects "ex[hbs.engine" instead of "exphbs".
+
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res)=>{
 
-    res.send('Working');
+    res.render('home/index');
+});
+
+app.get('/about', (req, res)=>{
+
+    res.render('home/about');
+});
+
+
+app.get('/login', (req, res)=>{
+
+    res.render('home/login');
+});
+
+app.get('/register', (req, res)=>{
+
+    res.render('home/register');
 });
 
 app.listen(4500, ()=>{
